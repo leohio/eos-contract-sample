c6ee0de189e668ffe506ce41d375aecb8247b43d
CoRe103 <hiruma@geomerlin.com>
2019-01-07 21:40:57 +0900

    delete sh file

--- cmnt/compile.sh
+++
@@ -1,1 +0,0 @@
-eosio-cpp cmnt.cpp -o cmnt.wasm -abigen
--- cmnt/set_contract.sh
+++
@@ -1,2 +0,0 @@
-endpoint="https://api-kylin.eosasia.one:443"
-cleos -u $endpoint set contract pcstoycashio ~/eos_contracts/sample/cmnt -p pcstoycashio@active
